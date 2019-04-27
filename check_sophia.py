17a9a2e13b7e125ce102d8f66894b8fda6f0cb93
Giorgos Sfikas <georges.sfikas@gmail.com>
2019-04-27 15:36:27 +0300

    Added option to remove punctuation

--- check_sophia.py
+++ check_sophia.py
@@ -2,23 +2,62 @@ import os
 import re
 import argparse
 import logging
+import tqdm
+import cv2
+import numpy as np
 
-def get_words_from_pagexml(xmlname):
+def get_list_of_unigrams():
+    raise NotImplementedError
+
+def process_bbox(xx):
+    xx = xx.split(' ')
+    res = []
+    for i in xx:
+        tt = i.split(',')
+        rj = []
+        for j in tt:
+            rj.append(int(j))
+        res.append(rj)
+    res = np.array(res)
+    res = cv2.boundingRect(res)
+    return(res)
+
+def get_words_from_pagexml(xmlname,
+        strip_punctuation=True,
+        force_lowercase=False):
     """
     Returns a list of tuples. Each tuple corresponds to one word.
     """
+    if force_lowercase is True:
+        raise NotImplementedError
     with open(xmlname, 'r') as f:
         xmldata = f.read().replace('\n', '')
     rexp = '<Word id="(.*?)">\s*<Coords points="(.*?)"/>\s*<TextEquiv>\s*<Unicode>(.+?)</Unicode>\s*</TextEquiv>\s*</Word>'
     words = re.findall(rexp, xmldata)
-    print('Found {} words in file {}.'.format(len(words), xmlname))
-    return(words)
+    #
+    words_processed = []
+    punctuation_mark_table = dict.fromkeys(map(ord, ',.’:;"-()!·'), None)
+    for x in words:
+        if strip_punctuation is True:
+            transcr_new = x[2].translate(punctuation_mark_table)
+        else:
+            transcr_new = x[2]
+        transcr_new = transcr_new.replace('&quot', '')
+        #trascr_new = re.sub(r'&quot', '', transcr_new)
+        points_new = process_bbox(x[1])
+        id_new = x[0]
+        if(len(transcr_new) == 0):
+            print('Warning! Found word with empty transcription (probably due to removed punctuation). Replacing with dummy character "#"')
+            transcr_new = '#'
+        words_processed.append( (id_new, points_new, transcr_new) )
+    print('Found {} words in file {}.'.format(len(words_processed), xmlname))
+    return(words_processed)
 
 if __name__=='__main__':
     logger = logging.getLogger('SophiaTrikoupi::dataset')
     logger.info('------')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', '-mode', required=True, choices=['check','read_words'], help='Execution mode.')
+    parser.add_argument('--mode', '-mode', required=True, choices=['check','extract_lexicon'], help='Execution mode.')
     args = parser.parse_args()
 
     all_word_tuples = []
@@ -32,7 +71,9 @@ if __name__=='__main__':
 
     if args.mode == 'check':
         print("Total words: {}".format(len(all_word_tuples)))
-    elif args.mode == 'read_words':
-        raise NotImplementedError
+    elif args.mode == 'extract_lexicon':
+        for x in all_word_tuples:
+            #print(x[0], x[1], x[2])
+            print(x[2])
     else:
         raise NotImplementedError
